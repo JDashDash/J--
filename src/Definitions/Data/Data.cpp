63214bd67b489d1412472dd9755cf5312bc1329e
Program132 <clement.gauthier132@orange.fr>
2023-08-03 01:17:50 +0200

    Data manager

---
+++ src/Definitions/Data/Data.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cleme on 03/08/2023.
+//
+
+#include "Data.h"
---
+++ src/Definitions/Data/Data.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+
+namespace JDD::Definition {
+    class Data {
+
+    };
+}
