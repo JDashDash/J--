e14aa3cc9ab17513874afca45f0a2ce0cc553010
Program132 <clement.gauthier132@orange.fr>
2023-08-03 01:21:44 +0200

    print & println done (1 value inside)

---
+++ src/LexerParser/Parser.cpp
@@ -0,0 +1,52 @@
+#include "Parser.h"
+
+namespace JDD::Parser {
+    void JDDParser::main(const std::vector<JDD::Lexer::Token>& tokenList) {
+        Definition::Data data;
+        auto current_token = tokenList.begin();
+
+        while (current_token != tokenList.end()) {
+            if (!ManagerInstruction(current_token, data)) {
+                std::cerr << "Unknown: " << current_token->content << std::endl;
+                ++current_token;
+            }
+        }
+    }
+
+    bool JDDParser::ManagerInstruction(std::vector<Lexer::Token>::const_iterator& current, Definition::Data& data) {
+        auto instruction = ExpectIdentifiant(current);
+        if (instruction.has_value() && instruction->content == "print") {
+            print(current, data, false);
+            return true;
+        }  if (instruction.has_value() && instruction->content == "println") {
+            print(current, data, true);
+            return true;
+        }
+        return false;
+    }
+
+    void JDDParser::print(std::vector<Lexer::Token>::const_iterator& current, Definition::Data& data, bool jumpline) {
+        auto openValues = ExpectOperator(current, "(");
+        if (!openValues.has_value()) {
+            std::cout << "forgot to open '(' and make sure to close with ')'" << std::endl;
+        }
+
+        auto value = ExpectValue(current);
+        if (!value.has_value()) {
+            std::cout << "forgot to give value in print" << std::endl;
+        }
+
+        auto closeValues = ExpectOperator(current, ")");
+        if (!closeValues.has_value()) {
+            std::cout << "forgot to close with ')'" << std::endl;
+        }
+
+        auto endInstruction = ExpectOperator(current, ";");
+        if (!endInstruction.has_value()) {
+            std::cout << "forgot to close the instruction with ';'" << std::endl;
+        }
+
+        if (jumpline) { std::cout << "\n"; }
+        std::cout << value->content;
+    }
+}
---
+++ src/LexerParser/Parser.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include "Lexer.h"
+
+#include "../Expecter/Expecter.h"
+#include "../Definitions/Data/Data.h"
+
+namespace JDD::Parser {
+    class JDDParser {
+    public:
+        static void main(const std::vector<JDD::Lexer::Token>& tokenList);
+
+    private:
+        static bool ManagerInstruction(std::vector<Lexer::Token>::const_iterator& current, Definition::Data& data);
+
+        // Instructions :
+        static void print(std::vector<Lexer::Token>::const_iterator& current, Definition::Data& data, bool jumpline);
+    };
+}
