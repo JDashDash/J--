cc86be43c04bf01b8fd919ebfe9b0bfabe895c09
Program132 <clement.gauthier132@orange.fr>
2023-08-03 09:33:29 +0200

    Added string module to data

--- src/Definitions/Data/Data.h
+++ src/Definitions/Data/Data.h
@@ -12,6 +12,10 @@ namespace JDD::Definition {
         std::map<std::string, Variable> Variables; // Variable Name, Variable Object
 
     public:
+        // Modules Enable
+        bool isStringModuleImported;
+
+        // Functions
         bool isVariable(const std::string& name);
         std::optional<Variable> getVariable(const std::string& name);
         void pushVariable(const Variable& var);
