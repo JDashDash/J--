0b99f781e4c793d9b620708c15e0c2eca381b750
Program132 <clement.gauthier132@orange.fr>
2023-08-03 10:04:26 +0200

    getCharFromIndex & getIndexFromChar added to String

--- src/Modules/String.h
+++ src/Modules/String.h
@@ -4,4 +4,6 @@
 
 namespace JDD::Modules::String {
     std::string concat(const std::string& str1, const std::string& str2);
+    int getIndexFromChar(const std::string& str, char x);
+    char getCharFromIndex(const std::string& str, int index);
 }
