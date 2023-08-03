b87c21d344bc4d660f35297f293201ff11313f7d
Program132 <clement.gauthier132@orange.fr>
2023-08-03 07:48:07 +0200

    Map to save vars

--- src/Definitions/Data/Data.cpp
+++ src/Definitions/Data/Data.cpp
@@ -1,5 +1,17 @@
-//
-// Created by cleme on 03/08/2023.
-//
-
 #include "Data.h"
+
+bool JDD::Definition::Data::isVariable(const std::string& name) {
+    if (this->Variables.contains(name))
+        return true;
+    return false;
+}
+
+std::optional<JDD::Definition::Variable> JDD::Definition::Data::getVariable(const std::string &name) {
+    if (isVariable(name))
+        return this->Variables[name];
+    return std::nullopt;
+}
+
+void JDD::Definition::Data::pushVariable(const JDD::Definition::Variable& var) {
+    this->Variables[var.name] = var;
+}
--- src/Definitions/Data/Data.h
+++ src/Definitions/Data/Data.h
@@ -2,9 +2,18 @@
 
 #include <iostream>
 #include <map>
+#include <optional>
+
+#include "../Variables/Variables.h"
 
 namespace JDD::Definition {
     class Data {
+    private:
+        std::map<std::string, Variable> Variables; // Variable Name, Variable Object
 
+    public:
+        bool isVariable(const std::string& name);
+        std::optional<Variable> getVariable(const std::string& name);
+        void pushVariable(const Variable& var);
     };
 }
