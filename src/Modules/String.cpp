6c0a05a52bbbc2a103ef045cbbaddb57bf0c111b
Program132 <clement.gauthier132@orange.fr>
2023-08-03 09:33:18 +0200

    Added first module + func

---
+++ src/Modules/String.cpp
@@ -0,0 +1,7 @@
+#include "String.h"
+
+namespace JDD::Modules::String {
+    std::string concat(const std::string& str1, const std::string& str2) {
+        return str1 + str2;
+    }
+}
---
+++ src/Modules/String.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace JDD::Modules::String {
+    std::string concat(const std::string& str1, const std::string& str2);
+}
