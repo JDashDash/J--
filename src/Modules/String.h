5363c59cd6f429c7f953c16fb160330851244736
Program132 <clement.gauthier132@orange.fr>
2023-08-03 12:20:58 +0200

    Moved + added functions

--- src/Modules/String.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace JDD::Modules::String {
-    std::string concat(const std::string& str1, const std::string& str2);
-    int getIndexFromChar(const std::string& str, char x);
-    char getCharFromIndex(const std::string& str, int index);
-}
---
+++ src/Modules/String/String.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <algorithm>
+
+namespace JDD::Modules::String {
+    std::string concat(const std::string& str1, const std::string& str2);
+    int getIndexFromChar(const std::string& str, char x);
+    char getCharFromIndex(const std::string& str, int index);
+    bool equals(const std::string& str1, const std::string& str2);
+    bool equalsIgnoreCase(const std::string& str1, const std::string& str2);
+    bool isEmpty(const std::string& str1);
+    std::string toLowerCase(const std::string& str1);
+    std::string toUpperCase(const std::string& str1);
+    std::string replace(const std::string& str1, char x, char y);
+}
