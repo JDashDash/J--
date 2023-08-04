d7e2fcf39c710445fee1d11f629a4b57701b2416
Program132 <clement.gauthier132@orange.fr>
2023-08-04 21:46:59 +0200

    REWORKED String Module

--- src/Modules/ModuleManager.cpp
+++ src/Modules/ModuleManager.cpp
@@ -10,14 +10,65 @@ void JDD::Modules::ModulesManager::useStringModule(std::vector<JDD::Lexer::Token
 
     if (functionString.has_value() && functionString->content == "concat") {
         auto nextString = ExpectValue(current, data);
-        if (!nextString.has_value() || nextString->type != Definition::STRING)
-            std::cerr << "'concat' function need string as first parameter" << std::endl;
+        if (!nextString.has_value())
+            std::cerr << "'concat' function need a value or can give a variable as first parameter" << std::endl;
         content = content + nextString->content;
+    } if (functionString.has_value() && functionString->content == "getIndexFromChar") {
+        auto nextString = ExpectValue(current, data);
+        if (!nextString.has_value() || nextString->type != Definition::STRING)
+            std::cerr << "'getIndexFromChar' function need a string value as first parameter" << std::endl;
+        content = std::to_string(JDD::Modules::String::getIndexFromChar(content, nextString->content[0]));
+    } if (functionString.has_value() && functionString->content == "getCharFromIndex") {
+        auto nextString = ExpectValue(current, data);
+        if (!nextString.has_value() || nextString->type != Definition::INT)
+            std::cerr << "'getCharFromIndex' function need a int value as first parameter" << std::endl;
+        content = std::to_string(JDD::Modules::String::getCharFromIndex(content, std::stoi(nextString->content)));
+    }  else if (functionString.has_value() && functionString->content == "equals") {
+        auto nextString = ExpectValue(current, data);
+        if (!nextString.has_value() || nextString->type != Definition::STRING)
+            std::cerr << "'equals' function need string as first parameter" << std::endl;
+        content = std::to_string(JDD::Modules::String::equals(content, nextString->content));
+    } else if (functionString.has_value() && functionString->content == "equalsIgnoreCase") {
+        auto nextString = ExpectValue(current, data);
+        if (!nextString.has_value() || nextString->type != Definition::STRING)
+            std::cerr << "'equalsIgnoreCase' function need string as first parameter" << std::endl;
+        content = std::to_string(JDD::Modules::String::equalsIgnoreCase(content, nextString->content));
+        if (content == "1") {
+            content = "true";
+        } else {
+            content = "false";
+        }
+    } else if (functionString.has_value() && functionString->content == "isEmpty") {
+        content = std::to_string(JDD::Modules::String::isEmpty(content));
+        if (content == "1") {
+            content = "true";
+        } else {
+            content = "false";
+        }
     } else if (functionString.has_value() && functionString->content == "toLowerCase") {
         content = JDD::Modules::String::toLowerCase(content);
     } else if (functionString.has_value() && functionString->content == "toUpperCase") {
         content = JDD::Modules::String::toUpperCase(content);
-    }
+    } else if (functionString.has_value() && functionString->content == "replace") {
+        auto firstChar = ExpectValue(current, data);
+        if (!firstChar.has_value() || firstChar->type != Definition::STRING)
+            std::cerr << "'replace' function need string value as first parameter" << std::endl;
+        auto secondChar = ExpectValue(current, data);
+        if (!secondChar.has_value() || secondChar->type != Definition::STRING)
+            std::cerr << "'replace' function need string value as first parameter" << std::endl;
+        content = JDD::Modules::String::replace(content, firstChar->content[0], secondChar->content[0]);
+        if (content == "1") {
+            content = "true";
+        } else {
+            content = "false";
+        }
+    } else if (functionString.has_value() && functionString->content == "valueOf") {
+        auto nextString = ExpectValue(current, data);
+        if (!nextString.has_value())
+            std::cerr << "'valueOf' function need a value or can give a variable as first parameter" << std::endl;
+        content = nextString->content;
+    } else
+        std::cerr << "This function from String is not available" << std::endl;
 
     if (!ExpectOperator(current, ")").has_value())
         std::cerr << "Close with ')' to conclude the operation(s)" << std::endl;
