64d666651f915214531d0969b5e73de073f9c0d2
Program132 <clement.gauthier132@orange.fr>
2023-08-07 11:45:27 +0200

    Created Double Module Functions

---
+++ src/Modules/Double/Double.cpp
@@ -0,0 +1,31 @@
+#include "Double.h"
+
+namespace JDD::Modules::Double {
+    double max(double x, double y) {
+        if (x > y)
+            return x;
+        return y;
+    }
+
+    double min(double x, double y) {
+        if (x < y)
+            return x;
+        return y;
+    }
+
+    double sum(double x, double y) {
+        return x + y;
+    }
+
+    double sub(double x, double y) {
+        return x - y;
+    }
+
+    double mul(double x, double y) {
+        return x * y;
+    }
+
+    double div(double x, double y) {
+        return x / y;
+    }
+}
---
+++ src/Modules/Double/Double.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+namespace JDD::Modules::Double {
+    double max(double x, double y);
+    double min(double x, double y);
+    double sum(double x, double y); // x + y
+    double sub(double x, double y); // x - y
+    double mul(double x, double y); // x * y
+    double div(double x, double y); // x * y
+}
