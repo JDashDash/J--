4040822efca8241d9719c7b15f694e326097d77c
Program <67320703+Program132@users.noreply.github.com>
2024-06-22 21:39:18 +0200

    Moved & update

--- src/Definitions/Value/Value.cpp
+++
@@ -1,6 +0,0 @@
-#include "Value.h"
-
-std::ostream &JDD::Definition::operator<<(std::ostream &flux, const JDD::Definition::Value &v) {
-    flux << "(Value: " << v.content << ", " << sAllTypes[v.type] << ")";
-    return flux;
-}
